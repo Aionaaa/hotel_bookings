10e7f4ff435e9bdab170f80633873da231874d77
Iona Grant <ionagrant92@gmail.com>
2019-10-30 11:24:46 +0000

    data displays kind of

---
+++ client/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+<p>{{bookings}}</p>
+  </div>
+</template>
+
+<script>
+import {eventBus} from './main';
+import BookingService from './services/BookingService';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      bookings: []
+    }
+  },
+  components: {
+
+  },
+  mounted(){
+    this.fetchData();
+  },
+  methods: {
+    fetchData(){
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
